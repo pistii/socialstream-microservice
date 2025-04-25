7a75e741b2813eeb2745510ef83554c372161744
pistii <pilisi.istvan@gmail.com>
2025-04-25 11:04:47 +0200

    Globalize BaseController

---
+++ services/shared-libraries/Controller/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared_libraries.Controller
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected int GetUserId()
+        {
+            int? userId = (int?)HttpContext.Items["UserId"];
+            if (userId == null)
+            {
+                throw new UnauthorizedAccessException("User not found in request headers.");
+            }
+            return (int)userId!;
+        }
+
+    }
+}
