ff4a5e3acee1048e209c2cc6ccade9c0ad65155d
pistii <pilisi.istvan@gmail.com>
2025-05-11 16:40:23 +0200

    ft: Configure grpc connections and DI grpc services for gateway api

--- gateway/Program.cs
+++ gateway/Program.cs
@@ -1,30 +1,61 @@
 using shared_libraries.Interfaces;
 using GrpcServices.Services;
-using notification_service.Repository;
+using GrpcServices.Clients;
+using GrpcServices.Interfaces;
+using GrpcServices.Protos;
+using Serilog;
+using GrpcServices;
+using gateway.Realtime;
 
+
+namespace gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
+            var configuration = builder.Configuration;
+            services.AddScoped<IUserGrpcClient, UserGrpcClient>();
+            services.AddScoped<IFriendGrpcClient, FriendGrpcClient>();
+            services.AddScoped<INotificationGrpcClient, NotificationGrpcClient>();
 
-builder.Services.AddControllers();
-builder.Services.AddGrpc();
             services.AddSignalR(options =>
             {
                 options.MaximumReceiveMessageSize = 102400000;
                 options.EnableDetailedErrors = true;
 
             });
+            //Grpc configuration
+            services.AddGrpc();
+            services.AddGrpcClient<GrpcNotification.GrpcNotificationClient>(o =>
+            {
+                o.Address = new Uri(configuration["GrpcServices:NotificationServiceUrl"]);
+            });
+            services.AddGrpcClient<User.UserClient>(o =>
+            {
+                o.Address = new Uri(configuration["GrpcServices:UserServiceUrl"]);
+            });
+            services.AddGrpcClient<Friend.FriendClient>(o =>
+            {
+                o.Address = new Uri(configuration["GrpcServices:FriendServiceUrl"]);
+            });
 
+            services.AddControllers();
 
-// Configure the HTTP request pipeline.
+            var app = builder.Build();
 
 app.MapGrpcService<NotificationService>();
 app.MapGrpcService<UserService>();
 app.MapGrpcService<FriendService>();
+
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllers();
             app.MapHub<NotificationHub>("/Notification");
             app.MapHub<ChatHub>("/Chat");
 app.Run();
+        }
+    }
+}
